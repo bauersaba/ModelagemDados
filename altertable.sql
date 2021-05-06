4b83ab855861221898203d3f20e35f201e15ac0d
Marcelo Bauer Sabaraense <bauersaba@gmail.com>
2021-05-05 22:26:48 -0300

    Update altertable.sql

--- altertable.sql
+++ altertable.sql
@@ -10,3 +10,20 @@ ALTER TABLE aluno drop column idadeAluno;
 alter table aluno modify column nomeAluno varchar (80) not null;
 
 alter table aluno change column nomeAluno nome_aluno varchar (30);
+
+
+CREATE table professor(
+	cpf int primary key,
+	nomeProfessor varchar (50),
+	idade int 
+
+);
+create table aluno(
+	idAluno int primary key auto_increment,
+	nomeAluno varchar (50) not null,
+	idade int not null
+);
+
+alter table aluno add fk_professor_cpf int;
+
+alter table aluno add foreign key (fk_professor_cpf) references professor(cpf);
